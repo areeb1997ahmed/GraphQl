7a806e0b2b59128650f472fed5439e05f2ba0566
Areeb Ahmed Khan <areeb1997ahmed@gmail.com>
2023-11-24 06:27:07 +0500

    Update index.js
    multiple request have created like for
    single User
    todosWith Their User id And name
    specific todos
    specific User

--- server/index.js
+++ server/index.js
@@ -1,37 +1,68 @@
-const express = require('express')
-const {ApolloServer} =require('@apollo/server')
-const cors = require('cors')
-const {expressMiddleware}=require('@apollo/server/express4')
-const bodyParser = require('body-parser')
-const axios = require('axios')
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const cors = require("cors");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-
-
-async function startServer (){
-    const app=express();
-    const server=new ApolloServer({
-        typeDefs:`
-            type Product{
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+        type User{
+            id:ID!
+           name:String!
+         }
+            type Todos{
+                userId:ID
                 id:ID!
                 title:String!
-                description:String!  
-        }
+                completed:Boolean
+               user:User!
+             }
+
         type Query {
-            getProducts:[Product]
+            getProducts:[Todos]
+            User:[User]
+            getUser(id:ID!):User
         } `,
-        resolvers:{
-            Query:{
-                getProducts:async ()=>(await axios.get("https://fakestoreapi.com/products")).data
-            }
-        }
-    });
+    resolvers: {
+      //this is for like join the table and getting the user according to the todos
+      //we do this because we dont want to execute the api multiple time
 
-    app.use(bodyParser.json());
-    app.use(cors());
+      // the attribute todos should be same as define is the schema on line 18
+      // the inner attribute `user` should be same as the key define in Todos key user
 
-    await server.start();
+      Todos: {
+        user: async (todos) =>
+          (
+            await axios.get(
+              `https://jsonplaceholder.typicode.com/users/${todos.userId}`
+            )
+          ).data,
+      },
+      Query: {
+        getProducts: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
 
-    app.use("/graphql", expressMiddleware(server));
-    app.listen(8000,()=> console.log('server started'));
+        User: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+
+        // this is for finding a specific user According to the parameter id
+
+        getUser: async (parent, { id }) =>
+          (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`))
+            .data,
+      },
+    },
+  });
+
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  await server.start();
+
+  app.use("/graphql", expressMiddleware(server));
+  app.listen(8000, () => console.log("server started"));
 }
 startServer();
